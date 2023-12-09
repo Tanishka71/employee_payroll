899d9a5bd93faa6e9c1d8dfb3c84d4dcc9b90af4
Tanishka Jain <tanishkajain1508@gmail.com>
2023-12-09 15:00:31 +0530

    ADDED:UC_2_CREATE_TABLE_FOR_EMPLOYEE_PAYROLL

--- employee_payroll.sql
+++ employee_payroll.sql
@@ -3,3 +3,12 @@ create database payroll_service;
 use payroll_service;
 SELECT database();
 SHOW databases;
+
+#<-----------USE CASE 2------------->
+CREATE TABLE employee_payroll (
+id INT unsigned NOT NULL AUTO_INCREMENT,
+name VARCHAR(150) NOT NULL,
+salary Double NOT NULL,
+start DATE NOT NULL,
+PRIMARY KEY (id)
+);
